c74b65262a66ee1c66c3fa9dc4262a033b0d1afe
PyCharm <alexander.thorunnarson@proton.me>
2026-03-31 08:47:51 +0000

    Updated config.py

--- config.py
+++ config.py
@@ -2,29 +2,27 @@
 k_P = 15
 k_I = 0
 k_D = 5
-
-# Other compensator parameters
 dt = 2
 
 # Camera resolution
 res = [1920, 1080]
 roi_res = 400
 
-# Ceckerboard search bounds
+# Chessboard search bounds
 check_start = [0, 257.0, 15.0]
 check_end = [39, 289.0, 15.0]
 check_mesh = [6, 6]
 
+# Printer specific parameters
 printer_ip = 'http://trident.local'
+min_width = 0.4 # mm
+max_width = 3 # mm
 
-min_width = 0.4
-max_width = 3
-
-# Pattern parameters
+# Test pattern parameters
 pat_shape = [[30.0, 30.0], [270.0, 260.0]]
-layer_height = 0.6
+layer_height = 0.6 # mm
 lines = 11
-default_width = 2.0
+default_width = 2.0 # mm
 print_speed = 10 # mm/s
 travel_speed = 300 # mm/s
 
